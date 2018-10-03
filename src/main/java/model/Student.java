c1a215b318f733ed36a89c5adbec8b0de460bcda
iaroslav.liabogov <iaroslav.liabogov@globallogic.com>
2018-10-03 19:14:29 +0200

    upd

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,17 @@
+package model;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String programme;
+    private List<String> courses;
+
+}
--- src/test/java/studentsInfo/CRUDTest.java
+++ src/test/java/studentsInfo/CRUDTest.java
@@ -1,21 +1,41 @@
 package studentsInfo;
 
 import io.restassured.http.ContentType;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+import model.Student;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Title;
 import org.junit.Test;
 import testBase.TestBase;
 
+@Slf4j
 public class CRUDTest extends TestBase {
+
+    static String firstName = "SMOKEUSER";
+    static String lastName = "SMOKEUSER";
+    static String programme = "ComputerScience";
+    static String email = "xyz@gmail.com";
 
     @Title("Create a new student")
     @Test
     public void createStudent() {
+
+        Student student_1 = new Student();
+        student_1.setFirstName(firstName);
+        student_1.setLastName(lastName);
+        student_1.setProgramme(programme);
+        student_1.setEmail(email);
+
         SerenityRest.given()
                 .contentType(ContentType.JSON)
                 .when()
-                .body("")
-                .post();
+                .body(student_1)
+                .post()
+                .then()
+                .log()
+                .all()
+                .statusCode(201);
 
 
     }
