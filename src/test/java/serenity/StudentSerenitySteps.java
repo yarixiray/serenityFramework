d0e257005b8fee9841a9fdbb9efb99b134deb3dd
yarixiray <takemycarcompany@gmail.com>
2018-10-07 21:01:02 +0200

    upd

--- src/test/java/serenity/StudentSerenitySteps.java
+++ src/test/java/serenity/StudentSerenitySteps.java
@@ -4,28 +4,94 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ValidatableResponse;
 import model.Student;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.annotations.Steps;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class StudentSerenitySteps {
 
+    @Step
     public ValidatableResponse createStudent(String firstName, String lastName, String email, String programme,
                                              List<String> courses){
 
-    Student student = new Student();
+        Student student = new Student();
         student.setFirstName(firstName);
         student.setLastName(lastName);
         student.setEmail(email);
         student.setProgramme(programme);
         student.setCourses(courses);
 
-    return SerenityRest
-            .rest()
-            .given()
-            .contentType(ContentType.JSON)
-            .when()
-            .body(student)
-            .post()
-            .then();
+        return SerenityRest
+                .rest()
+                .given()
+                .contentType(ContentType.JSON)
+                .when()
+                .body(student)
+                .post()
+                .then();
     }
+
+    @Step("Getting the student information with firstName")
+    public HashMap<String,Object> getStudentsInfoByFirstName(String firstName){
+
+        String p1 = "findAll{it.firstName =='";
+        String p2 = "'}.get(0)";
+
+        return
+                SerenityRest.given()
+                        .when()
+                        .get("/list")
+                        .then()
+                        .log()
+                        .all()
+                        .statusCode(200)
+                        .extract()
+                        .path(p1 + firstName + p2);
+
+    }
+
+    @Step
+    public ValidatableResponse updateStudent(int studentid, String firstName, String lastName, String email, String programme,
+                                             List<String> courses){
+
+        Student student = new Student();
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setEmail(email);
+        student.setProgramme(programme);
+        student.setCourses(courses);
+
+        return SerenityRest
+                .rest()
+                .given()
+                .contentType(ContentType.JSON)
+                .log()
+                .all()
+                .when()
+                .body(student)
+                .put("/" + studentid)
+                .then();
+    }
+    @Step("Deleting the student")
+    public void deleteStudent(int studentId){
+        SerenityRest
+                .rest()
+                .given()
+                .when()
+                .delete("/" + studentId);
+
+    }
+    @Step
+    public ValidatableResponse getStudentInfoByStudentId(int studentId){
+
+       return SerenityRest
+                .rest()
+                .given()
+                .when()
+                .get("/" + studentId)
+                .then();
+    }
+
 }
--- src/test/java/studentsInfo/CRUDTest.java
+++ src/test/java/studentsInfo/CRUDTest.java
@@ -4,11 +4,17 @@ import io.restassured.http.ContentType;
 import model.Student;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
+import org.apache.http.HttpConnection;
+import org.apache.http.protocol.HTTP;
+import org.apache.tools.ant.taskdefs.condition.Http;
+import org.eclipse.jetty.http.HttpStatus;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import serenity.StudentSerenitySteps;
 import testBase.TestBase;
 import utils.TestUtils;
 
@@ -29,6 +35,9 @@ public class CRUDTest extends TestBase {
     private static String email = TestUtils.getRandomValue() + "xyz@gmail.com";
     private static int studentId;
 
+    @Steps
+    private static StudentSerenitySteps steps;
+
 
     @Title("Create a new student")
     @Test
@@ -38,23 +47,8 @@ public class CRUDTest extends TestBase {
         courses.add("C++");
         courses.add("Python");
 
-        Student student_1 = new Student();
-        student_1.setFirstName(firstName);
-        student_1.setLastName(lastName);
-        student_1.setProgramme(programme);
-        student_1.setEmail(email);
-        student_1.setCourses(courses);
-
-        SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .when()
-                .body(student_1)
-                .post()
-                .then()
-                .log()
-                .all()
-                .statusCode(201);
-
+        steps.createStudent(firstName,lastName,email,programme,courses)
+             .statusCode(HttpStatus.CREATED_201);
 
     }
 
@@ -62,24 +56,8 @@ public class CRUDTest extends TestBase {
     @Test
     public void test02getStudents() {
 
-        String p1 = "findAll{it.firstName =='";
-        String p2 = "'}.get(0)";
-
-        HashMap<String, Object> value =
-                SerenityRest.given()
-                        .when()
-                        .get("/list")
-                        .then()
-                        .log()
-                        .all()
-                        .statusCode(200)
-                        .extract()
-                        .path(p1 + firstName + p2);
-
-        System.out.println(value);
-
+        HashMap<String,Object> value = steps.getStudentsInfoByFirstName(firstName);
         assertThat(value, hasValue(firstName));
-
         studentId = (int) value.get("id");
     }
 
@@ -90,25 +68,10 @@ public class CRUDTest extends TestBase {
         List<String> courses = new ArrayList<>();
         courses.add("C++");
         courses.add("Python");
-
         firstName = firstName + "_Updated";
-
-        Student student_1 = new Student();
-        student_1.setFirstName(firstName);
-        student_1.setLastName(lastName);
-        student_1.setProgramme(programme);
-        student_1.setEmail(email);
-        student_1.setCourses(courses);
-
-        SerenityRest.given()
-                .contentType(ContentType.JSON)
-                .when()
-                .body(student_1)
-                .put("/" + studentId)
-                .then()
-                .log()
-                .all()
-                .statusCode(200);
+        steps.updateStudent(studentId,firstName,lastName,email,programme,courses);
+        HashMap<String,Object> value = steps.getStudentsInfoByFirstName(firstName);
+        assertThat(value,hasValue(firstName));
     }
 
     @Title("Verify if the updated student was added to the application")
@@ -138,22 +101,9 @@ public class CRUDTest extends TestBase {
     @Title("Delete the student and verify if the student is deleted")
     @Test
     public void test05deleteStudent() {
+    steps.deleteStudent(studentId);
+    steps.getStudentInfoByStudentId(studentId).statusCode(HttpStatus.NOT_FOUND_404);
 
-        SerenityRest
-                .rest()
-                .given()
-                .when()
-                .delete("/" + studentId);
-
-        SerenityRest
-                .rest()
-                .given()
-                .when()
-                .get("/" + studentId)
-                .then()
-                .log()
-                .all()
-                .statusCode(404);
 
     }
 
