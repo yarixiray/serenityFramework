d0c0f376cd034990f44d83ce0a452978c6dd8d84
yarixiray <takemycarcompany@gmail.com>
2018-10-09 00:16:25 +0200

    upd

--- src/test/java/studentsInfo/CreateStudentsDataDrivenTest.java
+++ src/test/java/studentsInfo/CreateStudentsDataDrivenTest.java
@@ -1,16 +1,31 @@
 package studentsInfo;
 
+import lombok.Getter;
+import lombok.Setter;
 import net.serenitybdd.junit.runners.SerenityParameterizedRunner;
-import net.thucydides.core.annotations.Step;
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Title;
+import net.thucydides.junit.annotations.UseTestDataFrom;
+import org.eclipse.jetty.http.HttpStatus;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import serenity.StudentSerenitySteps;
 import testBase.TestBase;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@UseTestDataFrom("testData/studentInfo.csv")
 @RunWith(SerenityParameterizedRunner.class)
 public class CreateStudentsDataDrivenTest extends TestBase {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String programme;
+    private String course;
 
     @Steps
     StudentSerenitySteps steps;
@@ -18,6 +33,9 @@ public class CreateStudentsDataDrivenTest extends TestBase {
     @Title("Data driven test for adding multiple students to the Students App.")
     @Test
     public void createMultipleStudents() {
-        steps.createStudent(f)
+        List<String> courses = new ArrayList<>();
+        courses.add(course);
+        steps.createStudent(firstName, lastName, email, programme, courses)
+                .statusCode(HttpStatus.CREATED_201);
     }
 }
